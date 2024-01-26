6e048f36797d8af0ccb415aa65c9ba3c6611dde8
Akash V <149047737+akxshv13@users.noreply.github.com>
2024-01-26 17:37:22 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,25 @@
+
+class Array
+{
+	public static void main(String args[])
+	{
+		int a[] = new int[5];
+		a[0]=11;
+		a[1]=12;
+		a[2]=20;
+		a[3]=68;
+		a[4]=100;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+		
+		int b[] = {10,20,30,40,50};
+	
+	for(int i=0;i<=b.length-1;i++)
+	{
+		System.out.println(b[i]);
+	}
+	}
+}
